c31887b17e1cf434a32827507e19a850b7c50ec6
Prabhjyot Singh <me@p-s.tech>
2025-04-24 00:38:42 -0400

    Optional all and new messages

--- app/interfaces/ConversationBase.py
+++ app/interfaces/ConversationBase.py
@@ -7,5 +7,5 @@ class ConversationBase(SQLModel):
     id: int = Field(primary_key=True)
     referenced_by: str = Field(max_length=255)
     start_time: int | None = Field(default=int(datetime.datetime.now(datetime.UTC).timestamp()))
-    all_messages: str = Field(sa_column=Column(BLOB))
-    new_messages: str = Field(sa_column=Column(BLOB))
+    all_messages: str |None = Field(sa_column=Column(BLOB))
+    new_messages: str | None = Field(sa_column=Column(BLOB))
